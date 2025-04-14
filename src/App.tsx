e970f519146f54431caf0f490abb657a01e8f984
Egor Volg <duna9588@gmail.com>
2025-04-14 19:55:38 +0300

    Add header component with logo and styling
    - Created a header component in App.tsx that includes a logo and vertical line.
    - Added CSS styles for the header, logo, and container in App.css.
    - Introduced a new SVG logo file in the images directory.
    - Updated global styles in index.css for better layout and typography.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import "./App.css";
+import logo from "./images/logo.svg";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="app">
+      <header className="header">
+        <div className="container">
+          <div className="header__inner">
+            <a href="#" className="logo__container">
+              <img src={logo} alt="logo" />
+              <div className="vertical-line"></div>
+              <span className="logo__text">Официальный дилер Максимум</span>
+            </a>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
 }
 
 export default App;
