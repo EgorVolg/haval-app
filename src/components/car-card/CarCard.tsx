73859a85e85da7197a7bfcc66e9fe364cde99ad0
Egor Volg <duna9588@gmail.com>
2025-04-14 23:27:39 +0300

    Refactor CSS styles for layout consistency; implement grid display in CarList and CarCard components

--- src/components/car-card/CarCard.tsx
+++ src/components/car-card/CarCard.tsx
@@ -1,8 +1,19 @@
-import React from 'react' 
-import "./CarCard.module.css"
+import React from "react";
+import styles from "./CarCard.module.css";
 
 export const CarCard = () => {
   return (
-    <div>CarCard</div>
-  )
-}
+    <div className={styles.card}>
+      <img
+        className={styles.car__img}
+        src="https://photobank.maximum.expert/photo/fotobankN/664dc5f0204d55001d866bd6.jpg"
+        alt="car"
+      />
+      <div className={styles.car__info}>
+        <p className={styles.car__title}>Chery Tiggo 4 PRO Action MT</p>
+        <div className={styles.car__description}>1,5 / 113 Л.С. / MT5</div>
+      </div>
+      <button className={styles.card__btn}>Подробнее</button>
+    </div>
+  );
+};
--- src/components/car-list/CarList.tsx
+++ src/components/car-list/CarList.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import styles from "./CarList.module.css";
+import { CarCard } from "../car-card/CarCard";
 
 export const CarList = () => {
   return (
-    <div>
-      <div className={styles.list}></div>
+    <div className={styles.list}>
+      {[...Array(10)].map((_, index) => (
+        <CarCard key={index} />
+      ))}
     </div>
   );
 };
